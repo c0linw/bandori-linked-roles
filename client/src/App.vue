8bcbb2821f21e93cf68b803cd34b8193a4d7600d
c0linw <41310013+c0linw@users.noreply.github.com>
2023-04-08 11:27:16 -0700

    Add starter files for frontend

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
